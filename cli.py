3af61ede95a1e0211e9e3612c9f794f2b73059bf
s. rannou <mxs@sbrk.org>
2022-11-27 23:37:13 +0100

    feat: unified image names

--- cli.py
+++ cli.py
@@ -8,6 +8,7 @@ import os
 import re
 import subprocess
 import uuid
+import pathlib
 
 from wand.image import Image
 
@@ -15,6 +16,37 @@ from wand.image import Image
 @click.group()
 def main():
     pass
+
+
+def compress_if_needed(fp):
+    """Losslessly compress TIFF images if not yet done.
+    """
+    try:
+        with Image(filename=fp) as image:
+            click.echo(f'inspecting image {fp} format={image.format} compression={image.compression}')
+
+            if image.format == 'TIFF' and image.compression == 'no':
+                click.echo(f'compressing {fp}...')
+                command = ['magick', 'mogrify', '-compress', 'zip', fp]
+                subprocess.run(command)
+            else:
+                click.echo(f'skipped {fp} as it is already processed')
+
+    except Exception as e:
+        click.echo(f'unable to process image {fp}: {e}, skipped')
+
+
+def rename_if_needed(fp):
+    """Rename images to fit the uuid format if not yet done.
+    """
+    p = pathlib.Path(fp)
+    suffix = p.suffix
+    if suffix == '.tif':
+        name = p.stem
+        is_uuid = '^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$'
+        if not re.match(is_uuid, name):
+            dst = f'{p.parent}/{uuid.uuid4()}.tif'
+            os.rename(fp, dst)
 
 
 @main.command()
@@ -30,19 +62,8 @@ def filmify(path: str):
             filmify(fp)
             continue
 
-        try:
-            with Image(filename=fp) as image:
-                click.echo(f'inspecting image {fp} format={image.format} compression={image.compression}')
-
-                if image.format == 'TIFF' and image.compression == 'no':
-                    click.echo(f'compressing {fp}...')
-                    command = ['magick', 'mogrify', '-compress', 'zip', fp]
-                    subprocess.run(command)
-                else:
-                    click.echo(f'skipped {fp} as it is already processed')
-
-        except Exception as e:
-            click.warning(f'unable to process image {fp}: {e}, skipped')
+        compress_if_needed(fp)
+        rename_if_needed(fp)
         
 
 
